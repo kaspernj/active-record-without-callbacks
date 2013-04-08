1ed1d135db48c30a7f857ab357a4a756c2867023
Kasper Johansen <kj@gfish.com>
2013-04-08 10:00:23 +0200

    Cleaned up the code a lot. Transfered a non-complete test from Naoshi. Will finish it when I get an idea how to.

--- lib/active-record-without-callbacks.rb
+++ lib/active-record-without-callbacks.rb
@@ -2,35 +2,38 @@
 class ActiveRecordWithoutCallbacks
   TRUE_BLOCK = proc{ true }
   
-  def self.wo_callbacks(klass)
+  def self.wo_callbacks(args)
+    klass = args[:class]
+    @debug = args[:debug]
+    
     #Callback-types that we will clear or recompile.
     callbacks = [:create, :find, :initialize, :destroy, :commit, :rollback, :save, :touch, :update, :validate, :validation]
-    
-    #Callback-types.
     callback_types = [:before, :after]
+    previous_values = []
     
     #List of various possible callbacks.
-    takes = [:_create_callbacks, :_find_callbacks, :_initialize_callbacks, :_destroy_callbacks, :_commit_callbacks, :_rollback_callbacks, :_save_callbacks, :_touch_callbacks, :_update_callbacks, :_validate_callbacks, :_validation_callbacks, ]
-    
-    #Array containing previous values.
-    saved = []
+    takes = [:_create_callbacks, :_find_callbacks, :_initialize_callbacks, :_destroy_callbacks, :_commit_callbacks, :_rollback_callbacks, :_save_callbacks, :_touch_callbacks, :_update_callbacks, :_validate_callbacks, :_validation_callbacks]
     
     #Run inside exclusive, so we are sure no other thread suffers from the horrible hack, that we are about to do.
     Thread.exclusive do
       callbacks.each do |callback|
+        puts "Disabling callbacks for '#{callback}'." if @debug
+        
         #Method-name for callbacks to reset.
         callbacks_method_name = "_#{callback}_callbacks"
         
         #Store previous value for restoring later.
-        saved << {
+        previous_values << {
           :callbacks_method_name => callbacks_method_name,
           :value => klass.__send__(callbacks_method_name).clone
         }
         
         #Clear all callbacks.
-        klass.__send__("#{callbacks_method_name}").clear
+        puts "Clearing callbacks in '#{callbacks_method_name}'." if @debug
+        klass.__send__(callbacks_method_name).clear
         
         #Test that callbacks really was cleared.
+        puts "Checking if everything is okay for '#{callback}'." if @debug
         raise "Expected '#{callbacks_method_name}' to be empty but it wasnt: #{klass.__send__(callbacks_method_name)}" unless klass.__send__(callbacks_method_name).empty?
         
         #Trick ActiveRecord to recompile callbacks.
@@ -45,11 +48,15 @@ class ActiveRecordWithoutCallbacks
       
       begin
         #Run the given block, now that all callbacks are disabled.
+        puts "Yielding block." if @debug
         return yield
       ensure
+        puts "Restoring callbacks." if @debug
+        
         #Restore previous callbacks.
-        saved.each do |save|
-          klass.__send__("#{save[:callbacks_method_name]}=", save[:value])
+        previous_values.each do |previous_value|
+          method_name = "#{previous_value[:callbacks_method_name]}="
+          klass.__send__(method_name, previous_value[:value])
         end
         
         #Trick ActiveRecord to recompile old callbacks.
--- spec/active-record-without-callbacks_spec.rb
+++ spec/active-record-without-callbacks_spec.rb
@@ -2,6 +2,36 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "ActiveRecordWithoutCallbacks" do
   it "doesnt fail" do
-    #no tests
+    #The following code tests disabled callbacks.
+    
+    #Deal.after_create do
+    #  raise "Create WTF!?"
+    #end
+    
+    #Deal.after_save do
+    #  raise "Save WTF!?"
+    #end
+    
+    Deal.before_create do
+      raise "Before create!"
+    end
+    
+    Deal.after_create do
+      raise "After create!"
+    end
+    
+    ActiveRecordWithoutCallbacks.wo_callbacks(Deal) do
+      deal = Deal.new(:legacy_id => 999999)
+      
+      #puts deal.methods.sort
+      
+      deal.save! :validate => false
+      puts "Deal created as expected"
+    end
+    
+    deal = Deal.new(:legacy_id => 999999999)
+    
+    deal.save! :validate => false
+    puts "Second deal creation did not fail."
   end
 end
